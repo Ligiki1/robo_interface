e901be2713c7261fb0f86c3f7d9e10421d769af2
Ligiki1 <56873825+Ligiki1@users.noreply.github.com>
2020-07-31 13:19:04 +0200

    adding stuff

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,85 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@Ligiki1 
+lisaschulzke
+/
+hfg_exhibition
+1
+0
+0
+Code
+Issues
+Pull requests
+3
+Actions
+Projects
+Wiki
+Security
+Insights
+hfg_exhibition/src/App.vue
+@lisaschulzke
+lisaschulzke fixing bugs
+Latest commit a7d1126 1 hour ago
+ History
+ 1 contributor
+43 lines (37 sloc)  1010 Bytes
+  
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Interface msg=""/>
+    <Layout streamUrl="http://hal.hfg.design:50005/live-21080.mjpg">
+
+      <template v-slot:videocomponent></template>
+      <template v-slot:controlcomponent>
+        <Interface></Interface>
+      </template>
+      <template v-slot:queuecomponent>
+        <SimpleQueueComponent></SimpleQueueComponent>
+        <!-- <h1>BOTTOM_RIGHT</h1> -->
+      </template>
+
+    </Layout>
   </div>
 </template>
 
 <script>
-import Interface from './components/interface.vue'
-export default {
-  name: 'App',
-  components: {
-    Interface
-  }
-}
+  import Interface from './components/interface.vue';
+  import Layout from './submodules/base_layout/src/components/interface.vue'
+  import SimpleQueueComponent from './components/SimpleQueueComponent.vue'
+  export default {
+    name: 'App',
+    components: {
+      Interface,
+      Layout,
+      SimpleQueueComponent,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
--- src/components/interface.vue
+++ src/components/interface.vue
@@ -1,120 +1,182 @@
 <template>
-<Layout>
   <div>
-    <b-button v-b-toggle.sidebar-1> <span class="glyphicon glyphicon-menu-burger" aria-hidden="true">menu</span>
-    </b-button>
-    <b-sidebar id="sidebar-1" title="Sidebar" shadow>
-      <div class="px-3 py-2">
-        <div>
-          <ul> <a href="#1">first</a> </ul>
-          <ul> <a href="#2">second</a> </ul>
-          <ul> <a href="#3">third</a> </ul>
-          <ul> <a href="#4">fourth</a> </ul>
-        </div>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
-      </div>
-    </b-sidebar>
     <div>
       <div id="controlscontainer">
-        <strong>Connected: {{connected}}</strong><br><br><br>
+        <strong>Connected: {{connected}}</strong><br>
         <div class="half">
-          <h4>Presets</h4>
-          <button v-on:click="go()" :disabled="!connected || !amIActive">GO</button>
-          <button v-on:click="stop()" :disabled="!connected || !amIActive">STOP</button><br>
-          <!-- <b-button class="buttonLineNavi" v-on:click = "go ('send message', $event)" >start</b-button> -->
-          <!-- <b-button class="buttonLineNavi" v-on:click = "stop ('send message', $event)">stop</b-button> -->
+          <!-- <button class="buttonGo" v-on:click="go()" !$store.getters.amIActive>GO</button> -->
+          <button class="buttonGo" v-on:click="go()" :disabled="!connected || !$store.getters.amIActive">GO</button>
+          <button class="buttonStop" v-on:click="stop()" :disabled="!connected || !$store.getters.amIActive">STOP</button><br>
+            <hr/>
+            <!-- Override Key: <input type="text" v-model="overridePW" v-on:change="authorize"> -->
         </div>
-        <div class="half">
+        <!-- <div class="half">
           <h4>Current Waiting Queue {{this.currentTimer}}</h4>
           <SimpleQueue :currentQueue="currentQueue" :ownId="ownId" :ownName="clientName"></SimpleQueue>
-        </div>
+        </div> -->
       </div>
+      <b-button class="info" v-b-toggle.sidebar-1>Informationen</b-button>
+      <b-sidebar id="sidebar-1" title="Informationen" shadow>
+        <div class="px-3 py-2">
+          <b-img class="image" src="../assets/foto3.jpeg" fluid thumbnail></b-img>
+          <h2>Line Follower</h2>
+          <p>
+            Unser ferngesteuertes Kameraauto ist mit Infrarotsensoren ausgestattet, die zwischen weiß und schwarz
+            unterscheiden können. So kann dem Line Follower eine bestimmte Route durch Linien vorgegeben werden.
+          </p>
+          <br />
+          <h5>Aufbau</h5>
+          <p>
+            Für unser Projekt benötigten wir neben vier Rädern mit Gleichstrommotoren auch zwei Plexiglasplatten, die
+            unsere Installation tragen.
+            Für die Installation und die Umsetzung der Technik verwendeten wir einen Arduino für die Steuerung und eine
+            H-Brücke für die Verbindung zu den Motoren.
+            Um alles auf einem Interface steuerbar zu machen, benötigten wir einen Raspberry Pi, der zudem mit einer
+            Kamera ausgestattet ist,
+            der die übermittelten Daten vom Backend des Frontends abholt und an den Arduino übermittelt.
+          </p>
+          <b-img class="image" src="../assets/foto.jpeg" fluid></b-img>
+          <br />
+          <br />
+          <b-img class="image" src="../assets/foto2.jpeg" fluid></b-img>
+          <br />
+          <br />
+          <h5>Entwicklung</h5>
+          <p>
+            Bevor wir das Auto zusammengebaut haben, schauten wir zuerst nach dem Antrieb der Motoren. Anschließend
+            verlöteten und verbauten wir die Motoren an die Räder und bauten alle Autoteile zusammen.
+            Jetzt entwickelten wir Code, um das Auto ohne Sensor anzusteuern. Danach versuchten wir zunächst die
+            Sensordaten des Infrarotsensors auszulesen. Hier änderten wir den Code mithilfe einer while-Schleife, sodass
+            wir nur noch 1 oder 0 als Wert bekommen haben. So ist 0 der Wert für weiß und 1 für schwarz.
+            Auf dieser Grundlage erstellten wir den Code, der Sensor und Auto zusammenbrachten.
+            Zunächst fuhr der Line Follower nur auf der Geraden und konnte keine Kurven fahren, bis wir die Räder in
+            jedem einzelnen Fall des Abbiegens verschieden ansteuerten. So dreht bei einer Links-Kurve das rechte Rad
+            schneller und bei einer Rechts-Kurve das linke.
+          </p>
+          <br />
+          <video width="320" height="240" controls>
+            <source src="../assets/video1.mp4" type="video/mp4">
+          </video>
+          <br/>
+          <video width="320" height="240" controls>
+            <source src="../assets/video2.mp4" type="video/mp4">
+          </video>
+          <br/>
+          <b-img class="image" src="../assets/foto4.jpeg" fluid></b-img>
+          <br />
+        </div>
+      </b-sidebar>
     </div>
   </div>
 
-</Layout>
+
 </template>
 
 <script>
-  import SimpleQueue from "./SimpleQueueComponent"
-  import Layout from './submodules/base_layout/src/components/interface.vue';
-    export default {
-        components: {
-            SimpleQueue
-        },
-        methods: {
-            //CHANGEME: die Namen der Nachrichten die ihr mit emit() verschickt müssen mit dem Backend matchen
-            buttonClick: function (preset) {
-                console.log("Click button " + preset);
-                this.$socket.emit('preset', preset);
-            },
-            go: function () {
-                this.$socket.emit('move', 'forward');
-            },
-            stop: function () {
-                this.$socket.emit('move', 'stop');
-            }
-        },
-        sockets: {
-            connect: function () {
-                console.log('socket connected')
-                this.connected = true;
-                this.ownId = this.$socket.id
-                this.$socket.emit("register_front")
-            },
-            disconnect: function () {
-                console.log('socket disconnected')
-                this.connected = false;
-            },
-            //CHANGEME: die Namen der Nachrichten hier reinkommen müssen mit dem Backend matchen (Funktionsname = Nachrichtenname)
-            nsp_list: function (data) {
-                console.log("NSPs:" + data);
-            },
-            update_queue: function (data) {
-                this.currentQueue = data;
-            },
-            queue_ping: function() {
-                this.$socket.emit("queue_pong")
-            },
-            update_timer:function(data) {
-                this.currentTimer = data;
-            },
-            client_name: function(data) {
-                this.clientName = data;
-            }
-        },
-        data: function () {
-            return {
-                connected: false,
-                currentQueue: [],
-                ownId: "undefined",
-                clientName: "undefined",
-                currentTimer: 0,
-            }
-        },
-        computed: {
-            amIActive: function() {
-                if(this.currentQueue.length == 0) return false;
-                return this.currentQueue[1][0].id === this.ownId;
-            }
-        }
-    }
+  // import SimpleQueue from "./SimpleQueueComponent";
+  export default {
+    components: {
+      // SimpleQueue
+    },
+    methods: {
+      //CHANGEME: die Namen der Nachrichten die ihr mit emit() verschickt müssen mit dem Backend matchen
+      buttonClick: function (preset) {
+        console.log("Click button " + preset);
+        this.$socket.emit('preset', preset);
+      },
+      go: function () {
+        this.$socket.emit('move', 'forward');
+      },
+      stop: function () {
+        this.$socket.emit('move', 'stop');
+      },
+      authorize: function() {
+        this.$socket.emit('authorize', this.overridePW);
+      }
+    },
+    sockets: {
+      connect: function () {
+        console.log('socket connected')
+        this.connected = true;
+        this.ownId = this.$socket.id
+        this.$socket.emit("register_front")
+      },
+      disconnect: function () {
+        console.log('socket disconnected')
+        this.connected = false;
+      },
+      //CHANGEME: die Namen der Nachrichten hier reinkommen müssen mit dem Backend matchen (Funktionsname = Nachrichtenname)
+      nsp_list: function (data) {
+        console.log("NSPs:" + data);
+      },
+      authorized: function(data) {
+        this.authorized = data;
+      }
+    },
+    data: function () {
+      return {
+        connected: false,
+        overridePW: "",
+        authorized: false
+      }
+    },
+  }
 </script>
 
 <style scoped>
-  .like {
-    margin: 5em;
-    padding: 2em;
+  .buttonGo {
+    margin: 3vw;
+    margin-bottom: 2vw;
+    /* padding: 10 em; */
+    border-radius: 9%;
+    width: 8vw;
+    height: 5vh;
   }
-  .button {
-    margin: 2em;
+  .buttonGo:hover {
+    background-color: #7F9E38;
+    color: whitesmoke;
+  }
+  .buttonStop {
+    margin: 3vw;
+    margin-bottom: 2vw;
+    /* padding: 10 em; */
+    border-radius: 9%;
+    width: 8vw;
+    height: 5vh;
+  }
+  .buttonStop:hover {
+    background-color: #E88072;
+    color: whitesmoke;
+  }
+  p {
+    display: flex;
+    text-align: left;
+  }
+  .headline {
+    font-weight: bold;
+  }
+  .image {
+    border-radius: 15px;
+  }
+  .camerastream {
+    width: 500 vw;
+    height: 2000 vh;
+    border-style: solid;
+  }
+  strong {
+    margin: 1vw;
+  }
+  .info {
+    background-color: #0F404F;
+    color: whitesmoke;
+    border-style: none;
     padding: 0.5em;
+    font-weight: bold;
   }
-  .buttonLineNavi {
-    margin: 2em;
+  .info:hover {
+    background-color: #4C899C;
+    color: whitesmoke;
+    border-style: none;
+    padding: 0.5em;
   }
 </style>
